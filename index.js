8765a8f8375c3771fe62ada59eed44dbe1ead746
Emilse Arias <emilsearias@gmail.com>
2024-09-25 09:47:51 -0300

    agregamos los middlewares cors, morgan y los de express

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
-
+import cors from 'cors';
+import morgan from 'morgan';
 //1- configurar un puerto
 const app = express();
 
@@ -9,12 +10,16 @@ app.listen(app.get('port'), ()=>{
 })
 
 //2- configurar middlewares
+app.use(cors()); //permite conexiones remotas
+app.use(morgan('dev')); //nos da informacion extra en la terminal
+app.use(express.json()); // interpreta los datos del body en formato json
+app.use(express.urlencoded({extended:true})); //interpretar datos enviados de formularios
 
 //agregar como recibir conexiones remotas
 
 //3- configurar las rutas
 
 app.get('/prueba', (req, res)=>{
-    console.log('desde la funcion de prueba')
+    // console.log('desde la funcion de prueba')
     res.send('Desde el backend del proyecto crudCafe')
 })
