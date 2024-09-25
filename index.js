49a697048bd3de1cb879b76ddc15c5cc18f7e4e5
Emilse Arias <emilsearias@gmail.com>
2024-09-25 10:00:48 -0300

    agregamos el middleware para configurar un archivo estatico

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import path from 'path'
+import { fileURLToPath } from 'url';
+
 //1- configurar un puerto
 const app = express();
 
@@ -14,8 +17,12 @@ app.use(cors()); //permite conexiones remotas
 app.use(morgan('dev')); //nos da informacion extra en la terminal
 app.use(express.json()); // interpreta los datos del body en formato json
 app.use(express.urlencoded({extended:true})); //interpretar datos enviados de formularios
-
-//agregar como recibir conexiones remotas
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+// console.log(__filename)
+// console.log(__dirname)
+// console.log(path.join(__dirname,'/public'))
+app.use(express.static(path.join(__dirname,'/public'))) //configuramos un archivo estatico para ver el index.html en la ruta principal
 
 //3- configurar las rutas
 
