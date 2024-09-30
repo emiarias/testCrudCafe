154d9f86edd66795e4cb3647809210a1ea5abffb
Emilse Arias <emilsearias@gmail.com>
2024-09-30 10:43:15 -0300

    agregamos la logica para crear un producto y contestamos con error cuando el producto no pudo ser generado

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { leerPrueba } from "../controllers/productos.controllers.js";
+import { crearProducto, leerPrueba } from "../controllers/productos.controllers.js";
 
 // app.get('/prueba', (req, res)=>{
 //     // console.log('desde la funcion de prueba')
@@ -7,5 +7,6 @@ import { leerPrueba } from "../controllers/productos.controllers.js";
 // })
 const router = Router();
 router.route("/prueba").get(leerPrueba);
+router.route("/productos").post(crearProducto);
 
 export default router;
